9092b5754911c240c7dc6bbb0d3c046e003e2bff
DICKSON WAMBUA <Dickson_Local_AC@Air-Muoki-2.attlocal.net>
2022-01-08 22:12:45 -0500

    Moving to Database Setup

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,19 +2,25 @@ const express = require('express');
 //we do not use const app = express() because this would create a new app, instead, we use the method of express and call route in the .get instead of app.get and now you can import the route file into server.js and use the routes
 const route = express.Router();
 
+/**
+ * @description Root Route
+ * @method GET /
+ */
 const services = require('../services/render')
-route.get('/', (req, res) => {
-    //renders html is index.ejs. no need to specify ejs because it is initialized above
-    res.render('index');
-})
 
-route.get('/add-user', (req, res) => {
-    //renders add_user.ejs template
-    res.render('add_user');
-})
-route.get('/update-user', (req, res) => {
-    //renders add_user.ejs template
-    res.render('update_user');
-})
+route.get('/', services.homeRoutes)
+    /**
+     * @description add user
+     * @method GET / add_user
+     */
+
+route.get('/add-user', services.add_user);
+/**
+ * @description update user
+ * @method GET / update-user
+ */
+
+route.get('/update-user', services.update_user);
+
 
 module.exports = route;
