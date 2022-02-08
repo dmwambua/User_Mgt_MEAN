09d9cd27285e157274856e47b7459399fb7dd50b
DICKSON WAMBUA <Dickson_Local_AC@Air-Muoki-3.attlocal.net>
2022-02-08 15:11:11 -0500

    Added CRUD APIs

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -32,15 +32,64 @@ exports.create = (req, res) => {
 
 //retrieve and return all users/retrieve and return a single user
 exports.find = (req, res) => {
-
+    //getting a specific user
+    if (req.query.id) {
+        const id = req.query.id;
+        Userdb.findById(id)
+            .then(data => {
+                if (!data) {
+                    res.status(404).send({ message: "User not found with id" + id })
+                } else {
+                    res.send(data)
+                }
+            })
+            .catch(err => {
+                res.status(500).send({ message: "Error in retrieving user with id " + id })
+            })
+    }
+    //get data from db and return a response
+    Userdb.find()
+        .then(user => {
+            res.send(user)
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message || "Error occurred while retrieving user information" })
+        })
 }
 
 //Update a new identified user by user id
 exports.update = (req, res) => {
+    if (!req.body) {
+        return res
+            .status(400)
+            .send({ message: "Data to update cannot be blank" })
+    }
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: "Cannot update user with ${id}. User may not be existing" })
+            } else {
+                res.send(data)
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: "Error in updating user information" })
+        })
 
 }
 
+
 //Delete a user with a specified user id in the request
 exports.delete = (req, res) => {
-
+    const id = req.params.id;
+    Userdb.findByIdAndDelete(id)
+        .then(data => {
+            if (!data) { res.send(404).send({ message: "Cannot delete id ${id}. id my not be existing" }) } else {
+                res.send({ message: "User was deleted successfully" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: "Could not delete user with id = " + id });
+        });
 }
