b285213a9b9bba7ff3884ea81b3faaadee633093
DICKSON WAMBUA <Dickson_Local_AC@Air-Muoki-3.attlocal.net>
2022-02-09 21:33:48 -0500

    Updated EJS to add users

--- server/services/render.js
+++ server/services/render.js
@@ -7,7 +7,7 @@ exports.homeRoutes = (req, res) => {
     //make a get request to /api/users
     axios.get('http://localhost:3000/api/users')
         .then(function(response) {
-            console.log(response)
+            console.log(response.data)
             res.render('index', { users: response.data });
         })
         .catch(err => {
@@ -20,28 +20,3 @@ exports.add_user = (req, res) => {
 exports.update_user = (req, res) => {
     res.render('update_user');
 }
-
-/*
-const axios = require('axios');
-
-
-exports.homeRoutes = (req, res) => {
-    // Make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
-        .then(function(response) {
-            res.render('index', { users: response.data });
-        })
-        .catch(err => {
-            res.send(err);
-        })
-
-
-}
-
-exports.add_user = (req, res) => {
-    res.render('add_user');
-}
-exports.update_user = (req, res) => {
-    res.render('update_user');
-}
-*/
