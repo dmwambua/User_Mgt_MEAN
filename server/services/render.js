387b19fd2a4c45963ee5a6a62950d7507c6aae07
DICKSON WAMBUA <Dickson_Local_AC@Air-Muoki-3.attlocal.net>
2022-05-13 15:28:25 -0400

    Added Update User functionality

--- server/services/render.js
+++ server/services/render.js
@@ -7,7 +7,7 @@ exports.homeRoutes = (req, res) => {
     //make a get request to /api/users
     axios.get('http://localhost:3000/api/users')
         .then(function(response) {
-         
+
             res.render('index', { users: response.data });
         })
         .catch(err => {
@@ -18,5 +18,11 @@ exports.add_user = (req, res) => {
     res.render('add_user');
 }
 exports.update_user = (req, res) => {
-    res.render('update_user');
+    axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
+        .then(function(userdata) {
+            res.render("update_user", { user: userdata.data })
+        })
+        .catch(err => {
+            res.send(err);
+        })
 }
